ef17d360af7b0abedef0d054411a8f08f0cf46ec
ShanePringle2 <spring300@caledonian.ac.uk>
2024-11-07 20:21:49 +0000

    Altered code to hopefuly remove the invoke method only conditionally error

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -1,24 +1,30 @@
-
-import java.util.logging.Logger;
-// test to see if i fixed file strucutre issue
+import java.util.Scanner;
+import java.util.logging.*;
 
 public class Dec2Hex {
     private static final Logger logger = Logger.getLogger(Dec2Hex.class.getName());
 
     public static void main(String[] args) {
+        try {
+            FileHandler fileHandler = new FileHandler("dec2hex.log");
+            fileHandler.setFormatter(new SimpleFormatter());
+            logger.addHandler(fileHandler);
+        } catch (IOException e) {
+            logger.severe("Error: Unable to set up file handler for logging.");
+            return;
+        }
+
         if (args.length == 0) {
             logger.severe("Error: No input provided.");
             return;
         }
-        
+
         try {
             int input = Integer.parseInt(args[0]);
-            logger.info("Hexadecimal: " + Integer.toHexString(input));
+            logger.info(String.format("Hexadecimal: %s", Integer.toHexString(input)));
         } catch (NumberFormatException e) {
             logger.severe("Error: Input is not an integer.");
         }
     }
 }
-
-
 
